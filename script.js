edb38f5e9fdba7c1c0b0d3320c0e95304f9718c9
martin kuracka <m.kuracka@gmail.com>
2021-02-25 14:45:00 +0100

    update

--- script.js
+++ script.js
@@ -19,35 +19,36 @@ async function joke() {
         jokeResponse = fullJoke;
         sendJoke(fullJoke);
     } catch(err) {
-        console.log(err);
+        console.log(err)
         errorMessage.hidden = false;
       }
 }
 
 // Send text joke to speech API
-sendJoke = (fullJoke) => {
-    console.log('fulljoke z fetchu', fullJoke);
-    fetch('http://localhost:3000/', {
-        method: 'post',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify({
-        input: (fullJoke)
-        })
-    }).then(response => response.json()).catch(console.log('can not get the response data'))
-        .then(console.log(response))
-}
-// sendJoke = (jokeResponse) => {
-//     VoiceRSS.speech({
-//         key: 'd4bb3b3e69504439b7518ca9b6f461c7',
-//         src: jokeResponse,
-//         hl: 'en-us',
-//         v: 'Linda',
-//         r: 0,
-//         c: 'mp3',
-//         f: '44khz_16bit_stereo',
-//         ssml: false
-//     });
+
+// sendJoke = (fullJoke) => {
+//     console.log('sendjko funkcia', fullJoke)
+//     fetch('http://localhost:3001/', {
+//           method: 'post',
+//           headers: {'Content-type': 'application/json'},
+//           body: JSON.stringify({
+//           input: (fullJoke)
+//           })
+//       })
 // }
+
+sendJoke = (jokeResponse) => {
+    VoiceRSS.speech({
+        key: 'd4bb3b3e69504439b7518ca9b6f461c7',
+        src: jokeResponse,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
 
 //  Event listeners
 myButton.addEventListener('click', () => joke() );
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const VoiceRSSClient = require("voice-rss-client");
+const VoiceRSSWebApi = require('./node_modules//voice-rss/src/voice-rss-api');
 const app = express();
 let jokeInput;
+let audioToSend;
+let audio;
+
+// VoiceRSS
+// const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+const VoiceRSS = new VoiceRSSWebApi('d4bb3b3e69504439b7518ca9b6f461c7');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -11,21 +17,31 @@ app.use(bodyParser.json());
 app.post('/', (req, res) => {
     jokeInput = req.body.input;
     sendJoke(jokeInput);
-    // console.log(jokeInput)
-})
+    res.send(audio)
+    }
+)
 
 sendJoke = (jokeInput) => {
-    console.log('joke from sendJoke function:', jokeInput);
-    VoiceRSS.speech({
-        key: 'd4bb3b3e69504439b7518ca9b6f461c7',
-        src: jokeInput,
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0,
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
+    console.log('from sendjoke ffffffffuuu', jokeInput);
+    VoiceRSS.getAudio(jokeInput, {}, (err, audio) => {
+        if (err) console.error(err);
+        else audioToSend = audio;
+        console.log.apply(audioToSend)
     });
 }
 
-app.listen(3000)
+// sendJoke = (jokeInput) => {
+//     console.log('joke from sendJoke function:', jokeInput);
+//     VoiceRSS.speech({
+//         key: 'd4bb3b3e69504439b7518ca9b6f461c7',
+//         src: jokeInput,
+//         hl: 'en-us',
+//         v: 'Linda',
+//         r: 0,
+//         c: 'mp3',
+//         f: '44khz_16bit_stereo',
+//         ssml: false
+//     });
+// }
+
+app.listen(3001)
